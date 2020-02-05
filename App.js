ddf56c3594fff46e15c34aed7268d3a1f4fc066d
rudrokhanpro <rudrokhan.pro@gmail.com>
2020-02-06 00:59:23 +0100

    Implementation de la navigation par onglet

--- App.js
+++ App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
 
-  render() {
-    return (
-      <View>
-        <Text> App </Text>
-      </View>
-    );
+// Navigation
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs';
+
+// Icons
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+// Screens
+import Scan from './screens/Scan';
+import Historique from './screens/Historique';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+// Navigation routes
+const routes = {
+  Scan: Scan,
+  Historique: Historique,
+
+  // NOM_ONGLET: COMPOSANT_ONGLET
+};
+
+const defaultNavigationOptions = ({ navigation }) => {
+  return {
+
+    /** @returns Icône en fonction de l'onglet actif */
+    tabBarIcon: ({ focused, tintColor }) => {
+      let iconName;
+      const { routeName } = navigation.state;
+
+      switch (routeName) {
+        case 'Scan':
+          iconName = 'barcode-scan';
+          break;
+
+        case 'Historique':
+          iconName = 'history';
+          break;
+
+        /*
+        case 'NOM_ONGLET':
+          iconName = 'NOM_ICONE';
+          break;
+        */
+
+        default:
+          iconName = 'cat'
+          break;
+      }
+
+      return <Icon name={iconName} size={25} color={tintColor} />
+    }
   }
 }
+
+const TabNavigator = createBottomTabNavigator(
+  routes,
+  {
+    initialRouteName: 'Scan',
+    order: ['Historique', 'Scan'],
+
+    tabBarComponent: (props) => (
+      <BottomTabBar {...props} />
+    ),
+
+    defaultNavigationOptions,
+
+    tabBarOptions: {
+      activeTintColor: 'orange',
+      inactiveTintColor: 'grey',
+    }
+  }
+);
+
+const App = createAppContainer(TabNavigator);
+
+export default App;
--- README.md
+++ README.md
@@ -20,3 +20,26 @@ Requis:
 - iOS 10.0
 
 ```
+
+## Utiliser des icones
+
+Yakoi utilise des icones présents dans [React Native Vector Icons](https://github.com/oblador/react-native-vector-icons) plus précisemement _Material Community Icons_.
+
+- Modifier `android/app/build.gradle` et ajouter: 
+```
+project.ext.vectoricons = [
+    iconFontNames: [ 'MaterialIcons.ttf', 'MaterialCommunityIcons.ttf' ]
+]
+apply from: "../../node_modules/react-native-vector-icons/fonts.gradle"
+```
+
+- Utilisation:
+```javascript
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
+```
+
+## React Navigation
+
+[Documentation React Navigation](https://reactnavigation.org/docs/en/4.x/tab-based-navigation.html)
+
